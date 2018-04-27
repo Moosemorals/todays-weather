57db02eac11e430864223582e8345e0f134b25ff
Osric Wilkinson <gitmoose@fluffypeople.com>
2018-04-27 16:02:36 +0100

    Bendy lines

--- src/main/webapp/weather.js
+++ src/main/webapp/weather.js
@@ -10,9 +10,62 @@ const dayNames = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 const styles = {
     "T": "stroke: red",
     "Pp": "stroke: green",
+    "H": "stroke: yellow",
     "W": "stroke: #8c8cc8",
     "G": "stroke: #acacca; stroke-dasharray: 1%"
 };
+
+class Point {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    get magnitude() {
+        return Math.hypot(this.x, this.y);
+    }
+
+    /**
+     *
+     * @param {Point} p1 
+     * @param {Point} p2 
+     */
+    static subtract(p1, p2) {
+        return new Point(p1.x - p2.x, p1.y - p2.y);
+    }
+
+    static multiply(p1, dist) {
+
+        return new Point(dist / 3, dist * p1.y / (3 * p1.x));
+    }
+
+    static add(p1, p2) {
+        return new Point(p1.x + p2.x, p1.y + p2.y);
+    }
+
+    static unit(p1) {
+        return new Point(p1.x / p1.magnitude, p1.y / p1.magnitude);
+    }
+}
+
+function polyLine(points) {
+    //    const result = ["M", points[0].x, points[0].y, "C"];
+    const result = [];
+    for (let i = 1; i < points.length - 2; i += 1) {
+
+        let dist = (points[i + 1].x - points[i].x);
+        let v0 = Point.unit(Point.subtract(points[i + 1], points[i - 1]));
+        let w0 = Point.multiply(v0, dist);
+        let c0 = Point.add(points[i], w0);
+
+        let v1 = Point.unit(Point.subtract(points[i + 2], points[i]));
+        let w1 = Point.multiply(v1, dist);
+        let c1 = Point.subtract(points[i + 1], w1);
+
+        result.push(c0.x, c0.y, c1.x, c1.y, points[i + 1].x, points[i + 1].y);
+    }
+    return result;
+}
 
 function buildSVG(name) {
     var NS = "http://www.w3.org/2000/svg";
@@ -34,8 +87,17 @@ function extractWeather(json) {
     const fields = json["SiteRep"]["Wx"]["Param"];
     const periods = json["SiteRep"]["DV"]["Location"]["Period"];
 
-    const units = {};
-    const dataSets = { };
+    const units = {
+        "mph": {
+            min: Number.MAX_SAFE_INTEGER,
+            max: Number.MIN_SAFE_INTEGER
+        },
+        "C": {
+            min: Number.MAX_SAFE_INTEGER,
+            max: Number.MIN_SAFE_INTEGER
+        }
+    };
+    const dataSets = {};
     const dates = [];
 
     for (let i = 0; i < fields.length; i += 1) {
@@ -44,12 +106,12 @@ function extractWeather(json) {
         dataSets[f] = {
             name: fields[i]["$"],
             unit: u,
-            normalise: "Cmph".indexOf(u) !== -1,
-            parse: f !== "V",   // Don't parse visibility
-            scale: f === "U" ? 10 : 1,  // Only uv index scales
+            normalise: u in units,
+            parse: f !== "V", // Don't parse visibility
+            scale: f === "U" ? 10 : 1, // Only uv index scales
             data: []
         }
-   }
+    }
 
     for (let i = 0; i < periods.length; i += 1) {
         const period = periods[i];
@@ -69,8 +131,27 @@ function extractWeather(json) {
                 if (dataSets[f].parse) {
                     v = parseFloat(v);
                 }
+
+                const u = dataSets[f].unit;
+
+                if (u in units) {
+                    if (units[u].max < v) {
+                        units[u].max = v;
+                    }
+                    if (units[u].min > v) {
+                        units[u].min = v;
+                    }
+                }
+
                 dataSets[f].data.push(v * dataSets[f].scale);
             }
+        }
+    }
+
+    for (let f in dataSets) {
+        const u = dataSets[f].unit
+        if (u in units) {
+            dataSets[f].limits = units[u];
         }
     }
 
@@ -78,33 +159,25 @@ function extractWeather(json) {
     return dataSets;
 }
 
+function buildCircle(x, y, r, ...args) {
+    return buildSVG("circle", "cx", x, "cy", y, "r", r, ...args);
+}
 
 function drawDataSet(dataSet, which) {
-    const group = buildSVG("g", "style", styles[which] );
-    const path = [];
+    const group = buildSVG("g", "style", styles[which]);
 
-    let maxValue = Number.MIN_SAFE_INTEGER;
-    let minValue = Number.MAX_SAFE_INTEGER;
+    const range = dataSet.normalise ? dataSet.limits.max - dataSet.limits.min : 1;
 
-    const values = dataSet.data.map(v => {
-        if (v > maxValue) {
-            maxValue = v;
-        }
-        if (v < minValue) {
-            minValue = v;
-        }
-        return v;
-    });
+    let points = [];
 
-    const range = maxValue - minValue;
-
-    for (let i = 0; i < values.length; i += 1) {
-        const t = dataSet.normalise ? 100 * (values[i] - minValue) / range : values[i];
-        path.push(i === 0 ? "M" : "L");
-        path.push(i * 10);
-        path.push(t);
+    for (let i = 0; i < dataSet.data.length; i += 1) {
+        const v = dataSet.data[i];
+        const t = dataSet.normalise ? 100 * (v - dataSet.limits.min) / range : v;
+        points.push(new Point(i * 20, t));
     }
-    group.appendChild(buildPath(path));
+
+    const p2 = polyLine(points);
+    group.appendChild(buildPath(["M", points[0].x, points[0].y, "C", ...p2]));
 
     return group;
 }
@@ -152,9 +225,9 @@ function drawGraph(json) {
 
     graph.appendChild(buildAxes(dataSets.dates));
 
-    const dataLines = buildSVG("g", "stroke-linejoin", "round", "fill", "none", "stroke-width", "2");
+    const dataLines = buildSVG("g", "stroke-linejoin", "round", "fill", "none", "stroke-width", "0.75");
 
-    ["T", "Pp", "W", "G"].forEach(f => 
+    ["T", "Pp", "W", "G", "H"].forEach(f =>
         dataLines.appendChild(drawDataSet(dataSets[f], f))
     );
 
